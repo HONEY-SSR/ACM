63827060e263511d04740c9e0be573fb50462909
SSR <88606535+HONEY-SSR@users.noreply.github.com>
2022-02-16 15:25:37 +0800

    [69.x 的平方根]

---
+++ LeetCode/69. x 的平方根.c
@@ -0,0 +1,33 @@
+//
+//  69. x 的平方根.c
+//  LeetCode
+//
+//  Created by SSR on 2022/2/16.
+//
+
+#include <stdio.h>
+
+int mySqrt(int x);
+
+int main() {
+    
+    printf("%d\n", mySqrt(2147395599));
+    return 0;
+}
+
+int mySqrt(int x) {
+    int left = 0, right = x / 2 + 1;
+    while (left <= right) {
+        double middle = (left + right) / 2;
+        if (middle * middle == x) {
+            return middle;
+        }
+        else if (middle * middle < x) {
+            left = middle + 1;
+        }
+        else {
+            right = middle - 1;
+        }
+    }
+    return right;
+}
