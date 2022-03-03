c3fd70e0d50f2228de3ec05554bb407b103404a6
SSR <88606535+HONEY-SSR@users.noreply.github.com>
2022-03-03 19:54:35 +0800

    [95.不同的二叉搜索树]

---
+++ LeetCode/95. 不同的二叉搜索树 II.c
@@ -0,0 +1,29 @@
+//
+//  95. 不同的二叉搜索树 II.c
+//  LeetCode
+//
+//  Created by SSR on 2022/2/28.
+//
+
+#include <stdio.h>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+struct TreeNode** generateTrees(int n, int* returnSize){
+    
+}
